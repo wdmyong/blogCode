b20b8560740fcb2ff37b2fdb560ccfaec2da61fc
duanyong <duanyong@kuaishou.com>
2018-11-21 18:19:53 +0800

    first commit

---
+++ src/main/java/com/wdm/blogcode/BlogCodeApplication.java
@@ -0,0 +1,12 @@
+package com.wdm.blogcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCodeApplication.class, args);
+	}
+}
